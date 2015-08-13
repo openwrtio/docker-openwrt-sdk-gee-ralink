2e75eeaec263544006bfca879f88af8b6adafdd8
sinkcup <sinkcup@163.com>
2015-08-13 11:41:46 +0800

    change wget to curl, because hub.docker.com having a bug

--- dl.sh
+++ dl.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -ex
 
+ip=`nslookup downloads.openwrt.io 208.67.222.222 | grep Address | tail -n 1 | awk '{print $2}'`
+echo $ip
+
 mkdir -p dl
 cat dl.txt | while read line
 do
-    wget "http://downloads.openwrt.io/sources/"$line -O dl/$line
+    curl -o dl/$line -H 'Host: downloads.openwrt.io' http://$ip/sources/$line
 done
